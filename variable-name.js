008810624bb68bb5e438007125061fc76ec584de
RabeyaEty01 <etyrabeya01@gmail.com>
2021-08-01 23:55:16 +0600

    js added

---
+++ variable-name.js
@@ -0,0 +1,21 @@
+var name = 'Hero Alam';
+
+// reserved keyword not allowed
+var false = 15;
+var delete = 'delete';
+
+// name should be one word 
+var nayok name = 'tom hanks';
+
+// quote is not allowed
+ var "nayika" = "shabnoor" ;
+
+ var my-office-address = 'azimpur lalabagh';
+ var my_office_address = 'azimpur lalabagh';
+ var myOfficeAddress = 'azimpur lalabagh'; //this naming is allowed
+
+ var PIE =3.14;
+
+ var 999price = 25; //number should not be used first in a variable name
+
+ var userHomeAddress = 'katabon';  //camelCase
---
+++ variable.js
@@ -0,0 +1,14 @@
+var price = 56 ;
+// var name = value;
+var age = 21;
+var temperature = 37;
+var Name = 'Ety' ;
+var address = "lalbagh dhaka, azimpur 1205";
+
+var pass = true;
+var subscribe = false;
+
+console.log(price+age);
+console.log(Name);
+console.log(address);
+
