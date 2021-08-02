5840ea5edde063130dcd7c2e39d539d6b7ab5329
RabeyaEty01 <etyrabeya01@gmail.com>
2021-08-02 11:49:59 +0600

    index added

---
+++ mathematical-operation.js
@@ -0,0 +1,11 @@
+var onionPrice =43;
+var eggPrice = 20;
+var eggQuantity = 7;
+var moneyAvailable = 100;
+var orangePrice = 5;
+var orangeQuantity = moneyAvailable / orangePrice; // division
+var totaPrice = (eggPrice*eggQuantity); //multiplication
+console.log(onionPrice+eggPrice); //sum
+console.log(onionPrice-eggPrice); //difference
+console.log(totaPrice);
+console.log(orangeQuantity);
